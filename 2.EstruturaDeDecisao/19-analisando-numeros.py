ed77cdd9ed0cbb76f3cd99e1c1a3f32aa9ae8aba
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-07-04 01:02:22 -0300

    exe019

---
+++ 2.EstruturaDeDecisao/19-analisando-numeros.py
@@ -0,0 +1,18 @@
+# Exercício 19 
+
+# Faça um Programa que leia um número inteiro menor que 1000 e imprima a quantidade de centenas, 
+# dezenas e unidades do mesmo. Observando os termos no plural a colocação do "e", da vírgula entre outros. 
+
+# Exemplo:
+# 326 = 3 centenas, 2 dezenas e 6 unidades
+# 12 = 1 dezena e 2 unidades Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
+
+
+number = int(input('Digite um número menor que 1000: '))
+unity = number // 1 % 10
+ten = number // 10 % 10 
+hundred = number // 100 % 10
+thousand = number // 1000 % 10
+
+print(f'Analisando o número {number} temos:')
+print(f'{unity} unidades, {ten} dezenas, {hundred} centenas e {thousand} milhares.')
