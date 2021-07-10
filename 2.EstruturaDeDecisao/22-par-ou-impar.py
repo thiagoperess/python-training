18ac55449c8fc2a067b0a69537ae3796d985d37f
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-07-10 14:09:05 -0300

    exe022

---
+++ 2.EstruturaDeDecisao/22-par-ou-impar.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça um número inteiro e determine se ele é par ou impar. 
+
+numero = int(input('digite um número: '))
+
+if numero % 2 == 0:
+    print(f'O número {numero} é par')
+else:
+    print(f'O número {numero} é ímpar.')
