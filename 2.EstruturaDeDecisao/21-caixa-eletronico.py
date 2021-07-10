9a89fa80da9001ecfae05b4801ac18abc6f63dfd
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-07-10 04:38:52 -0300

    exe21

---
+++ 2.EstruturaDeDecisao/21-caixa-eletronico.py
@@ -0,0 +1,28 @@
+# Faça um Programa para um caixa eletrônico. 
+# O programa deverá perguntar ao usuário a valor do saque e depois informar quantas notas de cada valor serão fornecidas. 
+# As notas disponíveis serão as de 1, 5, 10, 50 e 100 reais. O valor mínimo é de 10 reais e o máximo de 600 reais. 
+# O programa não deve se preocupar com a quantidade de notas existentes na máquina.
+
+# Exemplo 1: Para sacar a quantia de 256 reais, o programa fornece duas notas de 100, uma nota de 50, uma nota de 5 e uma nota de 1;
+# Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 100, uma nota de 50, quatro notas de 10, uma nota de 5 e quatro notas de 1.
+
+saque = int(input('Qual valor você deseja sacar? R$ '))
+total = saque
+tot_cedula = 0
+cedula = 50
+while True:
+    if total >= cedula:
+        total -= cedula
+        tot_cedula += 1
+    else:
+        if tot_cedula > 0:
+            print(f'Total de {tot_cedula} cédula(s) de R$ {cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        tot_cedula = 0
+        if total == 0:
+            break
