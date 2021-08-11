440e1422182e50cfc3eeffe37877ce2a12a03127
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-11 11:25:35 -0300

    exe11 refactored

--- 1.EstruturaSequencial/11-analise-dados.py
+++ 1.EstruturaSequencial/11-analise-dados.py
@@ -2,21 +2,24 @@
 
 # Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
 
-# o produto do dobro do primeiro com metade do segundo;
-# a soma do triplo do primeiro com o terceiro;
-# o terceiro elevado ao cubo.
+# 1) o produto do dobro do primeiro com metade do segundo;
+# 2) a soma do triplo do primeiro com o terceiro;
+# 3) o terceiro elevado ao cubo.
 
 while True:
-    numberOne = int(input('Digite o 1º número: '))
-    numberTwo = int(input('Digite o 2º número: '))
-    numberThree = float(input('Digite o 3º número: '))
-    product = (numberOne * 2) * (numberTwo / 2)
-    valueSum = numberOne * 3 + numberThree
-    thirdCubed = numberThree ** 3
-    print(f'O produto entre o dobro de {numberOne} e a metade de {numberTwo} é {product:.0f}.')
-    print(f'A soma do triplo de {numberOne} e {numberThree} é {valueSum:.2f}.')
-    print(f'O número {numberThree} elevado ao cubo é {thirdCubed:.2f}.')
-    option = str(input('Deseja continuar? [S/N] ')).upper()
-    if option not in 'S':
+    num1 = int(input('Digite o 1º número: '))
+    num2 = int(input('Digite o 2º número: '))
+    num3 = float(input('Digite o 3º número: '))
+    
+    res1, res2, res3 = (num1 * 2) * (num2 / 2), num1 * 3 + num3, num3 ** 3
+
+    print(f'''O produto entre o dobro de {num1} e a metade de {num2} é {res1:.0f}.
+              A soma do triplo de {num1} e {num3} é {res2:.2f}.
+              O número {num3} elevado ao cubo é {res3:.2f}.
+              ''')
+
+    option = str(input('Deseja continuar? [S/N] ')).lower()
+    if option in 'n':
         break
+
 print('Até logo mais!!')
