e1d6d998dbf8c2f502ea2d555c87d59510628aea
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-14 10:32:45 -0300

    exe13 refactored

--- 1.EstruturaSequencial/13-peso-ideal-com-condicao.py
+++ 1.EstruturaSequencial/13-peso-ideal-com-condicao.py
@@ -6,18 +6,19 @@
 # Para homens: (72.7*h) - 58;
 # Para mulheres: (62.1*h) - 44.7.
 
-height = float(input('Digite sua altura: '))
-woman = 'F'
-man = 'M'
-gender = str(input('Digite seu sexo [F/M]: ')).upper()
-while gender not in 'FM':
-    gender = str(input('Digite seu sexo [F/M]: ')).upper()
-if gender in 'M':
-    print(f'Você tem {height:.2f}cm de altura.\n'
-          f'O peso ideal para um homem de sua altura é '
-          f'{(72.7 * height) - 58:.1f}Kg.')
+height = float(input('Qual sua altura? '))
+woman, man = 'f', 'm'
+
+gender = str(input('Qual sexo [F/M]? ')).lower()
+
+if gender in 'f':
+    print(f'Você tem {height:.2f}cm de altura. \
+            O peso ideal para uma mulher de sua altura é \
+            {(62.1  * height) - 44.7:.1f}Kg.')
+
 else:
-    print(f'Você tem {height:.2f}cm de altura.\n'
-          f'O peso ideal para uma mulher de sua altura é '
-          f'{(62.1  * height) - 44.7:.1f}Kg.')
+    print(f'Você tem {height:.2f}cm de altura. \
+            O peso ideal para um homem de sua altura é \
+            {(72.7 * height) - 58:.1f}Kg.')
+                
 print('Fim')
