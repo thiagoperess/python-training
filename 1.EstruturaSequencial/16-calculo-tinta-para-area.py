95baa713c2a0dbc7c37c110c7e2bb373fdaf0959
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-15 09:47:24 -0300

    exe16 refactored

--- 1.EstruturaSequencial/16-calculo-tinta-para-area.py
+++ 1.EstruturaSequencial/16-calculo-tinta-para-area.py
@@ -4,14 +4,21 @@
 # Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, 
 # que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
 
-metros = int(input('Metros quadrados a serem pintados: '))
-litros = metros / 3
+# metros = int(input('m² a serem pintados: '))
 
-precoL = 80.0
-capacidadeL = 18
+# litros = metros / 3
+# precoL, capacidadeL = 80.0, 18.0
+# latas = litros / capacidadeL
 
-latas = litros / capacidadeL
-total = latas * precoL
+# print(f'''Você usara {latas:.0f} latas de tinta
+#         O preco total é: R$ {latas*80:.2f}''')
 
-print (f'Você usara {latas:.0f} latas de tinta')
-print (f'O preco total é: R$ {total:.2f}')
+metros = float(input('m² a serem pintados: '))
+
+litros = metros / 3.0
+latas = int(litros / 18.0)
+if litros % 18 != 0:
+    latas += 1
+
+print(f'''Você usara {latas:.0f} latas de tinta
+        O preco total é: R$ {latas*80:.2f}''')
