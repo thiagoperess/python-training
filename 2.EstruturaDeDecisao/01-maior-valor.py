0a51be43385ad3a1f21f45a43feccb378c9ffed6
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-20 16:11:15 -0300

    exe02 refactored

--- 2.EstruturaDeDecisao/01-maior-valor.py
+++ 2.EstruturaDeDecisao/01-maior-valor.py
@@ -1,7 +1,12 @@
 # Exercício 01
 
-# Faça um Programa que peça dois números e imprima o maior deles. 
+# Faça um Programa que peça dois números e 
+# imprima o maior deles.Não usar a função 'max'.
 
 num1 = int(input('Primeiro número: '))
 num2 = int(input('Segundo número: '))
-print(max(num1, num2))
+
+if num1 > num2:
+    print(f'{num1} é maior que {num2}!')
+else:
+    print(f'{num2} é maior que {num1}!')
