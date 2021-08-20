eaa7130b17ecef6bd9b927796b8d4cb4adbe43d9
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-20 16:10:38 -0300

    exe18 refactored

--- 1.EstruturaSequencial/18-calculando-tempo.py
+++ 1.EstruturaSequencial/18-calculando-tempo.py
@@ -4,11 +4,11 @@
 # e a velocidade de um link de Internet (em Mbps), calcule e informe o 
 # tempo aproximado de download do arquivo usando este link (em minutos). 
 
-tamanho_arquivo = int(input('Qual o tamanho do arquivo? '))
-velocidade_download = int(input('Qual a velocidade? [Mbps] '))
+file_size = int(input('Tamanho do arquivo? '))
+download = int(input('Velocidade em [Mbps]: '))
 
-tempo_estimado = (tamanho_arquivo / velocidade_download) * 60
+time = (file_size / download) * 60
 
-print(f'A velocidade média de download para um arquivo de {tamanho_arquivo}MB com valocidade de {velocidade_download}Mbps é de {tempo_estimado:.0f} minutos')
+print(f'A velocidade download para {file_size}MB \
+        com {download}Mbps é de {time:.0f} minutos')
 
-# print('Tempo aproximado de download: %.0f Minutos ' %((tamanho_arquivo / velocidade_download) * 60))
