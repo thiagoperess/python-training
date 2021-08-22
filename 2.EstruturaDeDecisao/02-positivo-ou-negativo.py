0d21bab98f956db685ae7936f7f1edff7bc50b14
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-22 13:32:51 -0300

    exe02 refactored

--- 2.EstruturaDeDecisao/02-positivo-ou-negativo.py
+++ 2.EstruturaDeDecisao/02-positivo-ou-negativo.py
@@ -1,9 +1,6 @@
 # Exercício 02
 
-# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo. 
+# Faça um Programa que peça um valor e mostre 
+# na tela se o valor é positivo ou negativo. 
 
-valor = int(input('Digite o número: '))
-if valor < 0:
-    print(f'O número {valor} é negativo.')
-else:
-    print(f'O número {valor} é positivo.')
+print('Positivo' if int(input('Digite um número: ')) > 0 else 'Negativo')
