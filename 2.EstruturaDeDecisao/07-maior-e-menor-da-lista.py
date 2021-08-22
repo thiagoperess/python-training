a05481ca6f05c2bd88da1fb96c818c688d998c9c
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-22 13:36:54 -0300

    exe07 refactored

--- 2.EstruturaDeDecisao/07-maior-e-menor-da-lista.py
+++ 2.EstruturaDeDecisao/07-maior-e-menor-da-lista.py
@@ -2,10 +2,9 @@
 
 # Faça um Programa que leia três números e mostre o maior e o menor deles.
 
-list1 = list()
+list1 = []
 for i in range(0,3):
     num = int(input(f'Digite o {i+1}º número: '))
     list1.append(num)
-print(list1)
 print(f'O maior número é {max(list1)}.')
 print(f'O menor número é {min(list1)}.')
