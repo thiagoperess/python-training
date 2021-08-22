fe6dc3de8348746458d583aef776bc97a44654b4
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-22 13:33:13 -0300

    exe03 refactored

--- 2.EstruturaDeDecisao/03-validando-entradas.py
+++ 2.EstruturaDeDecisao/03-validando-entradas.py
@@ -1,12 +1,10 @@
 # Exercício 03
 
-# Faça um Programa que verifique se uma letra digitada é "F" ou "M". 
+# Faça um programa que verifique se uma letra digitada é "F" ou "M". 
 # Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido. 
 
-sexo = str(input('Digite o sexo: '))
-if sexo in 'Ff':
-    print('Feminino')
-elif sexo in 'Mm':
-    print('Masculino')
-else:
-    print('Sexo inválido')
+sexo = str(input('Sexo: ')).lower()
+
+if sexo in 'f': print('Feminino')
+elif sexo in 'm': print('Masculino')
+else: print('Sexo inválido')
