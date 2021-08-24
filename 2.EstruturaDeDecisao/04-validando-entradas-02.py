40fdbcf9b30fcdf539144452791a2a59bcee9d0f
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-24 12:44:36 -0300

    exe04 refactored

--- 2.EstruturaDeDecisao/04-validando-entradas-02.py
+++ 2.EstruturaDeDecisao/04-validando-entradas-02.py
@@ -1,5 +1,5 @@
 # Exercício 04
 
-# Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+# Faça um Programa que verifique se uma letra digitada é vogal ou consoante. Use apenas uma linha de código
 
 print('Vogal' if str(input('Digite a letra: ')) in 'AaEeIiOoUu' else 'Consoante')
