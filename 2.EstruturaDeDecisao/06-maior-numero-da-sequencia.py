79c95c47b12dd2510e30d484be06e1677dc7682f
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-25 23:01:04 -0300

    exe06 refactored

--- 2.EstruturaDeDecisao/06-maior-numero-da-sequencia.py
+++ 2.EstruturaDeDecisao/06-maior-numero-da-sequencia.py
@@ -1,9 +1,19 @@
 # Exercício 06
 
-# Faça um Programa que leia três números e mostre o maior deles. 
+# Faça um programa que leia três números e mostre o maior deles.
+# OBS: Não utilize a funções 'min' e 'max'.
 
-list1 = []
-for i in range(0,3):
-    num = int(input(f'Digite o {i+1}º número: '))
-    list1.append(num)
-print(f'O maior número é {max(list1)}.')
+lista = []
+for i in range(3):
+    num = int(input(f'{i+1}º número: '))
+    lista.append(num)
+
+maior = lista[0]
+
+if lista[0] < lista[1] > lista[2]:
+    maior = lista[1]
+
+elif lista[0] < lista[2] > lista[1]:
+    maior = lista[2]
+
+print(f'O maior valor digitado foi {maior}')
