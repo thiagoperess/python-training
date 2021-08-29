43efbddaa61ce07573d1aa9c393b3c7d88b69f79
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-29 01:00:04 -0300

    exe14 refactored

--- 2.EstruturaDeDecisao/14-calculo-media-aluno.py
+++ 2.EstruturaDeDecisao/14-calculo-media-aluno.py
@@ -1,6 +1,6 @@
 # Exercício 14
 
-# Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre,
+# Faça um programa que lê as N notas parciais obtidas por um aluno numa disciplina ao longo de um semestre,
 # e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
 
 # Média de Aproveitamento  Conceito
@@ -11,29 +11,29 @@
 # Entre 4.0 e 6.0          D
 # Entre 4.0 e zero
 
-# O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem:
+# O algoritmo deve mostrar na tela as notas, a média, 
+# o conceito correspondente e a mensagem:
 
 # “APROVADO” se o conceito for A, B ou C ou
 # “REPROVADO” se o conceito for D ou E.
 
-nota1 = float(input('Nota 1: '))
-nota2 = float(input('Nota 2: '))
-media = (nota1+nota2)/2
 
-print(f'Com media {media}, seu ', end='')
+lista = []
+qty_notas = int(input('Quantas notas? '))
 
-if 9 <= media < 10:
-    print('Conceito é A.')
-elif 7.5 <= media < 9:
-    print('Conceito é B.')
-elif 6 <= media < 7.5:
-    print('Conceito é C.')
-elif 4 <= media < 6:
-    print('Conceito é D.')
-elif media < 4:
-    print('Conceito é E.')
+for i in range(qty_notas):
+    notas = int(input(f'Digite a {i+1}ª nota: '))
+    lista.append(notas)
 
-if media >= 6:
-    print('APROVADO. PARABÉNS!')
-else:
-    print('REPROVADO. ESTUDE MAIS!')
+media = sum(lista) / len(lista)
+
+print(f'Notas: {lista}\
+        Média {media:.1f}')
+
+if 9 <= media <= 10: print('Conceito A.')
+elif 7.5 <= media < 9: print('Conceito B.')
+elif 6 <= media < 7.5: print('Conceito C.')
+elif 4 <= media < 6: print('Conceito D.')
+elif media < 4: print('Conceito E.')
+
+print('Aprovado!' if media >= 6 else 'Reprovado!')
