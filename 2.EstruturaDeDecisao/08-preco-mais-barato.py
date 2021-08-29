0f1db92e44163b296ea4e28ae72c36a7142ded85
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-29 00:57:58 -0300

    exe08 refactored

--- 2.EstruturaDeDecisao/08-preco-mais-barato.py
+++ 2.EstruturaDeDecisao/08-preco-mais-barato.py
@@ -1,10 +1,20 @@
 # Exercício 08
 
-# Faça um programa que pergunte o preço de três produtos e informe qual 
+# Faça um programa que pergunte o preço de N produtos e informe qual 
 # produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
 
-list1 = list()
-for i in range(0, 3):
-    produto = float(input(f'Qual o preço do produto R$ '))
-    list1.append(produto)
-print(f'O produto mais barato custa R$ {min(list1):.2f}')
+lista = []
+
+for i in range(3):
+    produto = float(input(f'Preço do {i+1}º produto R$ '))
+    lista.append(produto)
+
+menor = lista[0]
+
+if lista[0] > lista[1] < lista[2]:
+    menor = lista[1]
+
+elif lista[0] > lista[2] < lista[1]:
+    menor = lista[2]
+
+print(f'O produto mais barato custa R$ {menor:.2f}')
