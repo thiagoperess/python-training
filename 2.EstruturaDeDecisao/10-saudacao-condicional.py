18c2695022df57d556b5b4cf4b819c070cd56719
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-08-29 00:58:52 -0300

    exe10 refactored

--- 2.EstruturaDeDecisao/10-saudacao-condicional.py
+++ 2.EstruturaDeDecisao/10-saudacao-condicional.py
@@ -1,13 +1,19 @@
 # Exercício 10
 
-# Faça um Programa que pergunte em que turno você estuda. Peça para digitar 
-# M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", 
-# "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso. 
+# Faça um Programa que pergunte em que turno você estuda. 
+# Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. 
+# Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" 
+# ou "Valor Inválido!", conforme o caso. 
 
-turno = str(input('Em que turno você estuda? ')).upper()
-if turno in 'Mm':
-    print('Bom Dia')
-elif turno in 'Vv':
-    print('Boa Tarde')
-else:
-    print('Boa Noite')
+turno = str(input('Em que turno você estuda? ')
+).upper()
+
+while turno not in 'MVN':
+
+    turno = str(input('Opção Inválida: ')
+    ).upper()
+
+if turno in 'Mm': print('Bom Dia')
+elif turno in 'Vv': print('Boa Tarde')
+elif turno in 'N': print('Boa noite')
+    
