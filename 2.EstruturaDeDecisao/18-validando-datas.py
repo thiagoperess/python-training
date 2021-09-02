84320e3871267f929fab2bd6b9630afb7809437f
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-02 16:56:13 -0300

    exe18 refactored

--- 2.EstruturaDeDecisao/18-validando-datas.py
+++ 2.EstruturaDeDecisao/18-validando-datas.py
@@ -1,30 +1,40 @@
 # Exercício 18
 
-# Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a monthma é uma data válida.
+# Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
 
-day = int( input('Dia: ') )
-month = int( input('Mês: ') )
-year = int( input('Ano: ') )
+dia = int(input('Dia: '))
+mes = int(input('Mês: '))
+ano = int(input('Ano: '))
 
 valid = False
 
-if( month==1 or month==3 or month==5 or month==7 or \
-    month==8 or month==10 or month==12):
-    if(day<=31):
-        valid = True
+if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+    if dia <= 31: valid = True
 
-elif( month==4 or month==6 or month==9 or month==11):
-    if(day<=30):
-        valid = True
+elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+    if dia <= 30: valid = True
         
-elif month==2:
-    if (year%4==0 and year%100!=0) or (year%400==0):
-        if(day<=29):
-            valid = True
-    elif(day<=28):
-            valid = True
+elif mes == 2:
+    if ano %  4 == 0 and ano % 100 !=0 or ano % 400 == 0:
+        if dia <= 29: valid = True
+    elif dia <= 28: valid = True
 
-if(valid):
-    print('Data válida')
-else:
-    print('Inválida')
+print('Data válida' if valid else 'Inválida')
+
+# Utilizando datetime
+
+import datetime
+
+dataCorreta = None
+d = int(input('Dia: '))
+m = int(input('Mês: '))
+a = int(input('Ano: '))
+
+try:
+    novaData = datetime.datetime(a, m, d)
+    dataCorreta = True
+
+except ValueError:
+    dataCorreta = False
+
+print(str(dataCorreta))
