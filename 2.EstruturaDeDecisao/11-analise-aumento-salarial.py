cae3ff7c4bacaa61ac63cbaef246a85b73268ca1
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-02 08:37:02 -0300

    exe11 refactored

--- 2.EstruturaDeDecisao/11-analise-aumento-salarial.py
+++ 2.EstruturaDeDecisao/11-analise-aumento-salarial.py
@@ -1,36 +1,42 @@
 # Exercício 11
 
-# As Organizações Tabajara resolveram dar um plus de salário aos seus colaboradores 
-# e lhe contraram para desenvolver o programa que calculará os reajustes. Faça um programa
-#  que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
+# As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores 
+# e lhe contraram para desenvolver o programa que calculará os reajustes. 
+# Faça um programa que recebe o salário de um colaborador e o reajuste segundo 
+# o seguinte critério, baseado no salário atual:
 
-# salários até R$ 280,00 (incluindo) : plus de 20%
-# salários entre R$ 280,00 e R$ 700,00 : plus de 15%
-# salários entre R$ 700,00 e R$ 1500,00 : plus de 10%
-# salários de R$ 1500,00 em diante : plus de 5% Após o plus ser realizado, informe na tela:
+# salários até R$ 280,00 (incluindo) : aumento de 20%
+# salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+# salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+# salários de R$ 1500,00 em diante : aumento de 5%.
+ 
+# Após o aumento ser realizado, informe na tela:
 
 # o salário antes do reajuste;
-# o percentual de plus aplicado;
-# o valor do plus;
-# o novo salário, após o plus. 
+# o percentual de aumento aplicado;
+# o valor do aumento;
+# o novo salário, após o aumento. 
 
-salary = int(input('Salário: '))
+cash = int(input('Salário: R$ '))
 
-if salary <= 280:
+if cash <= 280:
     perc = 20
-    plus = salary * perc / 100 
 
-elif 280 < salary <= 700:
-    perc = 10
-    plus = salary * perc / 100
+elif 280 < cash < 700:
+    perc = 15
 
-elif salary > 700:
+elif 700 <= cash <= 1500:
     perc = 10
-    plus = salary * perc / 100
+
+elif cash > 1500:
+    perc = 5
+
+plus = cash * perc / 100
         
-print(f'''
-          Salário: R$ {salary:.2f}.
-          Aumento em %: {perc}.
-          Aumento em R$ {plus:.2f}.
-          Salário Total: R$ {salary + plus:.2f}.
+print(f'''Salário: R$ {cash:.2f}. 
+          Aumento em {perc}%, 
+          sendo R$ {plus:.2f}. 
+          Total: R$ {cash + plus:.2f}.
           ''')
+
+          
