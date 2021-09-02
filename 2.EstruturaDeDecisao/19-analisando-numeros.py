24313f5a348754a0c2d8b444a0889bd6745e1d69
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-02 16:56:26 -0300

    exe19 refactored

--- 2.EstruturaDeDecisao/19-analisando-numeros.py
+++ 2.EstruturaDeDecisao/19-analisando-numeros.py
@@ -8,11 +8,16 @@
 # 12 = 1 dezena e 2 unidades Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
 
 
-number = int(input('Digite um número menor que 1000: '))
-unity = number // 1 % 10
-ten = number // 10 % 10 
-hundred = number // 100 % 10
-thousand = number // 1000 % 10
+num = int(input('Digite um número menor que 1000: '))
 
-print(f'Analisando o número {number} temos:')
-print(f'{unity} unidades, {ten} dezenas, {hundred} centenas e {thousand} milhares.')
+unity = num // 1 % 10
+ten = num // 10 % 10 
+hundred = num // 100 % 10
+thousand = num // 1000 % 10
+
+print(f'Analisando o número {num} temos:')
+
+print(f'{unity} unidades, \
+        {ten} dezenas, \
+        {hundred} centenas e \
+        {thousand} milhares.')
