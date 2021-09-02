353e8a0eab58bd48bbd04d64ef50efe73fe027b4
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-02 16:58:26 -0300

    exe23 refactored

--- 2.EstruturaDeDecisao/23-inteiro-ou-decimal.py
+++ 2.EstruturaDeDecisao/23-inteiro-ou-decimal.py
@@ -3,9 +3,6 @@
 # Faça um Programa que peça um número e informe se o número é inteiro ou decimal.
 
 number = float(input('Digite o número: '))
-if number == round(number):
-    print(f'O número {number:.0f} é inteiro!')
-else:
-    print(f'O número {number} é decimal!')
 
+print(f'{number:.0f} é inteiro.' if number == round(number) else f'{number:.2f} é decimal.')
 
