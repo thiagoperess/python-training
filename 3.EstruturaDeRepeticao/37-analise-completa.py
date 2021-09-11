399f250aee6ea5dcf525be8391ff1f734783299b
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-11 17:05:21 -0300

    exe37 refactored

--- 3.EstruturaDeRepeticao/37-analise-completa.py
+++ 3.EstruturaDeRepeticao/37-analise-completa.py
@@ -7,18 +7,36 @@
 # Ao encerrar o programa também deve ser informados os códigos e valores do clente mais alto, 
 # do mais baixo, do mais gordo e do mais magro, além da média das alturas e dos pesos dos clientes
 
-costumers = int(input('Quantos clientes? '))
-
 codList = []
 heightList = []
 weightList = []
+cNum = 1
+code = True
 
-for i in range(costumers):
-    print(f'{i+1}º Cliente')
-    cod = int(input('Código: '))
-    height = float(input('Altura: '))
-    weight = float(input('Peso: '))
+while code != 0:
+    print(f'Cliente nº {cNum}')
+    code = int(input('Digite o código: '))
+    
+    if code == 0:
+        break
+    else:
+        height = float(input('Digite a altura: '))
+        weight = float(input('Digite o peso: '))
+        codList.append(code)
+        heightList.append(height)
+        weightList.append(weight)
+        cNum += 1
 
-    codList.append(cod)
-    heightList.append(height)
-    weightList.append(weight)
+thin = weightList.index(min(weightList))
+fat = weightList.index(max(weightList))
+tall = heightList.index(max(heightList))
+short = heightList.index(min(heightList))
+
+print(
+    f'Código do mais magro: {codList[thin]}\n'
+    f'Código do mais gordo: {codList[fat]}\n'
+    f'Código do mais alto: {codList[tall]}\n'
+    f'Código do mais baixo: {codList[short]}\n'
+    f'Média de altura: {sum(heightList) / len(heightList):.2f}\n'
+    f'Média de peso: {sum(weightList) / len(weightList):.2f}'
+    )
