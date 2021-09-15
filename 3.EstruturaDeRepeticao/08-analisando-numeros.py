362356f60210352883c934497fb943f2da8bda50
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-15 15:09:34 -0300

    exe08 refactored

--- 3.EstruturaDeRepeticao/08-analisando-numeros.py
+++ 3.EstruturaDeRepeticao/08-analisando-numeros.py
@@ -1,23 +1,19 @@
 # Exercício 08
 
-# Faça um programa que leia 5 números e informe a soma e a média dos números.
+# Faça um programa que leia 5 números 
+# e informe a soma e a média dos números.
 
 # Solução
 
-list1 = ['Numero 1: ',
-         'Numero 2: ', 
-         'Numero 3: ', 
-         'Numero 4: ', 
-         'Numero 5: ']
+lista = []
 
-list2 = []
-
-for i in range(len(list1)):
-    print(list1[i], end='')
+for i in range(5):
+    print(f'Número {i+1}: ', end='')
     
     nums = int(input(''))
-    list2.append(nums)
+    lista.append(nums)
 
-    average = sum(list2) / len(list2)
+    average = sum(lista) / len(lista)
     
-print(f'A soma dos números totaliza {sum(list2)} e sua média é {average:.0f}')
+print(f'Soma total: {sum(lista)}.\
+        Média: {average:.0f}')
