138cc2b5f0253b0225f1c35d128423f77c5d589d
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-15 15:07:20 -0300

    exe18 refactored

--- 2.EstruturaDeDecisao/18-validando-datas.py
+++ 2.EstruturaDeDecisao/18-validando-datas.py
@@ -6,35 +6,35 @@ dia = int(input('Dia: '))
 mes = int(input('Mês: '))
 ano = int(input('Ano: '))
 
-valid = False
+valida = False
 
 if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
-    if dia <= 31: valid = True
+    if dia <= 31: valida = True
 
 elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
-    if dia <= 30: valid = True
+    if dia <= 30: valida = True
         
 elif mes == 2:
     if ano %  4 == 0 and ano % 100 !=0 or ano % 400 == 0:
-        if dia <= 29: valid = True
-    elif dia <= 28: valid = True
+        if dia <= 29: valida = True
+    elif dia <= 28: valida = True
 
-print('Data válida' if valid else 'Inválida')
+print('Data válida' if valida else 'Inválida')
 
 # Utilizando datetime
 
-import datetime
+# import datetime
 
-dataCorreta = None
-d = int(input('Dia: '))
-m = int(input('Mês: '))
-a = int(input('Ano: '))
+# dataCorreta = None
+# d = int(input('Dia: '))
+# m = int(input('Mês: '))
+# a = int(input('Ano: '))
 
-try:
-    novaData = datetime.datetime(a, m, d)
-    dataCorreta = True
+# try:
+#     novaData = datetime.datetime(a, m, d)
+#     dataCorreta = 'Data válida'
 
-except ValueError:
-    dataCorreta = False
+# except ValueError:
+#     dataCorreta = 'Data inválida'
 
-print(str(dataCorreta))
+# print(str(dataCorreta))
