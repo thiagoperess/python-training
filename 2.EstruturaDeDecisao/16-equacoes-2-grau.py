4c67ed7c4ffb61203c07920b34245f88d6b48187
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-15 15:07:08 -0300

    exe16 refactored

--- 2.EstruturaDeDecisao/16-equacoes-2-grau.py
+++ 2.EstruturaDeDecisao/16-equacoes-2-grau.py
@@ -11,27 +11,50 @@
 # delta = b**2 - (4*a*c)
 # raiz = (-b +ou- (delta**(1/2)))/(2*a)
 
-a = float(input("Digite o valor de A: "))
-b = float(input("Digite o valor de B: "))
-c = float(input("Digite o valor de C: "))
+import math
 
-delta = 0
-raizUm = 0
-raizDois = 0
+print('Equação do 2º grau da forma: ax² + bx + c')
+
+a = int( input('Coeficiente a: ') )
 
 if a == 0:
-	print("A equação apresentada não é do segundo grau. O programa será encerrado!")
-
+	print('Se a = 0, não é equação do segundo grau.')
 else:
-	print("Calculando delta...")
-	delta = ((b ** 2) - (4 * a * c))
-	
-	if delta == 0:
-		print("A equação possui apenas uma raiz que é: ")
-		raizUm = ((-b) + (delta ** (1 / 2)) / (2 * a))
-		print(raizUm)
-	else:
-		raizUm = ((-b) + (delta ** (1 / 2)) / (2 * a))
-		raizDois = ((-b) - (delta ** (1 / 2)) / (2 * a))
-		print("A equação possui duas raizes que são:", raizUm, "e", raizDois)
+	b = int( input('Coeficiente b: ') )
+	c = int( input('Coeficiente c: ') )
+	delta = b * b - (4 * a * c)
 
+	if delta < 0:
+		print('Delta menor que 0. Raízes imaginárias.')
+	elif delta == 0:
+		raiz = -b / (2 * a)
+		print(f'Delta = 0 , raiz = {raiz}')
+	else:
+		raiz1 = (-b + math.sqrt(delta) ) / (2 * a)
+		raiz2 = (-b - math.sqrt(delta) ) / (2 * a)
+		print(f'Raízes: {raiz1} e {raiz2}')
+
+
+# a = float(input("Digite o valor de A: "))
+# b = float(input("Digite o valor de B: "))
+# c = float(input("Digite o valor de C: "))
+
+# delta = 0
+# raizUm = 0
+# raizDois = 0
+
+# if a == 0:
+# 	print("A equação apresentada não é do segundo grau. O programa será encerrado!")
+
+# else:
+# 	print("Calculando delta...")
+# 	delta = ((b ** 2) - (4 * a * c))
+	
+# 	if delta == 0:
+# 		print("A equação possui apenas uma raiz que é: ")
+# 		raizUm = ((-b) + (delta ** (1 / 2)) / (2 * a))
+# 		print(raizUm)
+# 	else:
+# 		raizUm = ((-b) + (delta ** (1 / 2)) / (2 * a))
+# 		raizDois = ((-b) - (delta ** (1 / 2)) / (2 * a))
+# 		print("A equação possui duas raizes que são:", raizUm, "e", raizDois)
