c6d4b28d8607acf2d7c71311d35944b7b204dd47
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-15 15:08:10 -0300

    exe24 refactored

--- 2.EstruturaDeDecisao/24-analise-numero-completa.py
+++ 2.EstruturaDeDecisao/24-analise-numero-completa.py
@@ -7,7 +7,6 @@
 # positivo ou negativo;
 # inteiro ou decimal.
 
-print('Digite dois números')
 n1 = float(input('Número 1: '))
 n2 = float(input('Número 2: '))
 
@@ -20,30 +19,28 @@ opt = int(input('''Qual operação deseja realizar?
 
 >>> '''))
 
-def add(x, y): return x + y
-def subt(x, y): return x - y
-def mult(x, y): return x * y
-def div(x, y): return x / y
-
 while True:
     if opt == 1:
-        num = add(n1, n2)
+        num = n1 + n2
 
     elif opt == 2:
-        num = subt(n1, n2)
+        num = n1 - n2
 
     elif opt == 3:
-        num = mult(n1, n2)
+        num = n1 * n2
 
     elif opt == 4:
-        num = div(n1, n2)
+        num = n1 / n2
 
     else:
         print('\nOpção Inválida!')
         break
 
     print(f'Total: {num:.1f}')
+
     print('É positivo' if num >= 0 else 'É negativo')
+    
     print('É inteiro' if num == round(num) else 'É decimal')
+    
     print('É par' if num % 2 == 0 else 'É ímpar')
     break
