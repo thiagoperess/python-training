8f6d86f8d8786b9e6fee299546e7375bfcdbdc05
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-15 15:08:54 -0300

    exe28 refactored

--- 2.EstruturaDeDecisao/28-comprando-carnes.py
+++ 2.EstruturaDeDecisao/28-comprando-carnes.py
@@ -7,68 +7,55 @@
 # Alcatra         R$ 5,90 por Kg          R$ 6,80 por Kg
 # Picanha         R$ 6,90 por Kg          R$ 7,80 por Kg
 
-# Para atender a todos os clientes, cada cliente poderá levar apenas um dos tipos de carne da promoção, porém não há limites para a quantidade de carne por cliente. 
-# Se a compra for feita no cartão Tabajara o cliente receberá ainda um desconto de 5% sobre o total da compra.
-
 # Escreva um programa que peça o tipo e a quantidade de carne comprada pelo usuário e gere um cupom fiscal, 
 # contendo as informações da compra: tipo e quantidade de carne, preço total, tipo de pagamento, valor do desconto e valor a pagar.
+# OBS: Se a compra for feita no cartão Tabajara o cliente receberá ainda um desconto de 5% sobre o total da compra.
+
 
 # Solução
 
-print('\nBem Vindo ao Mercado Tabajara')
-print('-' * 30)
-beef = int(input('Qual carne você quer?\n1 - Filé Duplo\n2 - Alcatra\n3 - Picanha\nOpção: '))
+print('Bem Vindo ao Mercado Tabajara')
+
+beef = int(input('''Qual carne você quer?
+
+1 - Filé Duplo
+2 - Alcatra
+3 - Picanha
+
+Opção: '''))
+
 qty_beef = int(input('Quantos kg você deseja? '))
-tabajara_card = str(input('Vai comprar com o Cartão Tabajara? [S/N] ')).lower().strip()
+card = str(input('Usa o Cartão? [S/N] ')).lower().strip()
 
 
 if beef == 1:    
-    filet = 0
     beef = 'Filé Duplo'
-
-    if qty_beef <= 5:
-        filet = 4.90
-    else:
-        filet = 5.80
+    filet = 4.90 if qty_beef <= 5 else 5.80
     total = filet * qty_beef
     
 elif beef == 2:
     beef = 'Alcatra'
-    rump = 0
-
-    if qty_beef <= 5:
-        rump = 5.90
-    else:
-        rump = 6.80
+    rump = 5.90 if qty_beef <= 5 else 6.80
     total = rump * qty_beef
     
-else:
-    steak = 0
+elif beef == 3:
     beef = 'Picanha'
-    
-    if qty_beef <= 5:
-        steak = 6.90
-    else:
-        steak = 7.80
+    steak = 6.90 if qty_beef <= 5 else 7.80
     total = steak * qty_beef
 
 discount = 0
 
-if tabajara_card == 's':
+if card == 's':
     discount = total * 0.05 
     total = total - discount
 
-print('-' * 32)
-
 print(f'''MERCADO TABAJARA - CUPOM FISCAL
---------------------------------
-Carne: {beef};
-Peso: {qty_beef} Kg;
-Total sem desconto R$ {total + discount:.2f};
-Usou o Cartão Tabajara? {tabajara_card};
-Desconto de R$ {discount:.2f};
-Total com desconto R$ {total:.2f}.''')
 
-print('-' * 32)
+Carne: {beef}
+Peso: {qty_beef}Kg
+Total sem desconto R${total + discount:.2f}
+Usou o Cartão Tabajara? {card}
+Desconto de R${discount:.2f}
+Total com desconto R${total:.2f}''')
+
 print('Obrigado e volte sempre!')
-print('-' * 32)
