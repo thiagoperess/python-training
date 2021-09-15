3f5a297c8608520e9c78a7efe632a9fc3cca3767
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-15 15:07:59 -0300

    exe23 refactored

--- 2.EstruturaDeDecisao/23-inteiro-ou-decimal.py
+++ 2.EstruturaDeDecisao/23-inteiro-ou-decimal.py
@@ -4,5 +4,5 @@
 
 number = float(input('Digite o número: '))
 
-print(f'{number:.0f} é inteiro.' if number == round(number) else f'{number:.2f} é decimal.')
+print('é inteiro.' if number == round(number) else 'é decimal.')
 
