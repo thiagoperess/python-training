bf1b5e2566c55edf039fdfe2078bf792746ca7de
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-22 07:51:40 -0300

    exe02 refactored

--- 3.EstruturaDeRepeticao/02-validando-senha.py
+++ 3.EstruturaDeRepeticao/02-validando-senha.py
@@ -1,15 +1,14 @@
 # Exercício 02
 
-# Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, 
-# mostrando uma mensagem de erro e voltando a pedir as informações.
+# Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, e voltando a pedir as informações.
 
 # Solução
 
-while True:
-	name = str(input('Digite seu nome: '))
-	password = str(input('Digite sua senha: '))
-	if name == password:
-		print('A senha não pode ser igual ao seu nome!')
-	else:
-		print('Seus dados foram salvos!')
-		break
+name, password = '', ''
+
+while name == password:
+	name = str(input('Digite o nome: '))
+	password = str(input('Digite a senha: '))
+
+else:
+	print('Seus dados foram salvos')
