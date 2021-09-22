4046be03f050c4efb5449771d3f064390139a6f8
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-22 07:53:10 -0300

    exe08 refactored

--- 3.EstruturaDeRepeticao/08-analisando-numeros.py
+++ 3.EstruturaDeRepeticao/08-analisando-numeros.py
@@ -1,19 +1,20 @@
 # Exercício 08
 
-# Faça um programa que leia 5 números 
+# Faça um programa que leia N números 
 # e informe a soma e a média dos números.
 
 # Solução
 
 lista = []
+qty_num = int(input('Quantos números? '))
 
-for i in range(5):
+for i in range(qty_num):
     print(f'Número {i+1}: ', end='')
-    
     nums = int(input(''))
+    
     lista.append(nums)
 
-    average = sum(lista) / len(lista)
+average = sum(lista) / len(lista)
     
-print(f'Soma total: {sum(lista)}.\
-        Média: {average:.0f}')
+print(f'Soma total: {sum(lista)}. '
+      f'Média: {average:.1f}.')
