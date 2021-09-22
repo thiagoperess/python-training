0e2fc287ef76161aec24714e9fe24fc89d22d80c
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-22 07:54:03 -0300

    exe35 refactored

--- 3.EstruturaDeRepeticao/35-listando-primos.py
+++ 3.EstruturaDeRepeticao/35-listando-primos.py
@@ -4,9 +4,12 @@
 # Faça um programa que gera uma lista dos números primos existentes 
 # entre 1 e um número inteiro informado pelo usuário.
 
+from time import sleep
+
 num = int(input('Qual número: '))
 
 for num in range(1, num + 1):
     if all(num % i != 0
         for i in range(2, num)):
             print(num, end='\n')
+            sleep(1)
