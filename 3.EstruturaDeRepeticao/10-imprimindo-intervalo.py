4ff253c1feaa45500f5dfed45ba707972f14ac15
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-28 18:19:04 -0300

    exe10 refactored

--- 3.EstruturaDeRepeticao/10-imprimindo-intervalo.py
+++ 3.EstruturaDeRepeticao/10-imprimindo-intervalo.py
@@ -1,11 +1,14 @@
 # Exercício 10
 
-# Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
+# Faça um programa que receba dois números e gere uma lis dos números que estão no intervalo compreendido por eles.
 
 # Solução:
 
-var1 = int(input('Início: '))
+var1 = int(input('Início: '))+1
 var2 = int(input('Fim: '))
+lista = []
 
-for i in range(var1+1, var2):
-    print(i)
+for i in range(var1, var2):
+    lista.append(i)
+
+print(lista)
