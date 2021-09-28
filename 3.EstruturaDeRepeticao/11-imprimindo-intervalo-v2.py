3bb07f15a2f94be79a12044d7122667ec5168d0d
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-09-28 18:19:18 -0300

    exe11 refactored

--- 3.EstruturaDeRepeticao/11-imprimindo-intervalo-v2.py
+++ 3.EstruturaDeRepeticao/11-imprimindo-intervalo-v2.py
@@ -1,18 +1,23 @@
 # Exercício 11
 
-# Altere o programa anterior para mostrar no final a soma dos números.
+# Altere o programa anterior para mostrar no final a soma dos números. No caso do início do intervalo ser menor que o final, o usuário deverá ser informado e solicitado a a digitar novamente.
 
 # Solução
 
 vai = int(input('Início: '))
-vaf = int(input('Fim: '))
+vaf = int(input('Final: '))
+
+while vai > vaf:
+    print('O início maior que o final!')
+    vai = int(input('Início: '))
+    vaf = int(input('Fim: '))
 
 lista = []
 soma = 0
 
 for i in range(vai+1, vaf):
-    print(i, end=' ')
     lista.append(i)
     soma += i
 
-print(f'A soma é {soma}')
+print(f'Lista: {lista}\n'
+        f'Soma total: {soma}')
