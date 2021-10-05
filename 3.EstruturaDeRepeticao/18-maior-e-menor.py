063f9aef1ef7c0921853fcfcde0a9bc77db7e929
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-10-05 14:19:43 -0300

    exe18 refactored

--- 3.EstruturaDeRepeticao/18-maior-e-menor.py
+++ 3.EstruturaDeRepeticao/18-maior-e-menor.py
@@ -3,15 +3,17 @@
 # Faça um programa que, dado um conjunto de N números,
 # determine o menor valor, o maior valor e a soma dos valores.
 
-list1 = []
-len_list1 = int(input('Tamanho da lista: '))
+lista = []
+qty_num = int(input('Quantidade de números: '))
 
-for i in range(len_list1):
+for i in range(qty_num):
     nums = int(input(f'Digite o {i+1}º número: '))
-    list1.append(nums)
+    lista.append(nums)
 
 print(f'''
-       O maior número é {max(list1)}
-       O menor número é {min(list1)}
-       E sua soma total é {sum(list1)}
-       ''')
+
+O maior número é {max(lista)}
+O menor número é {min(lista)}
+E sua soma total é {sum(lista)}
+
+''')
