d6d061ea7e1b2693b224f55b1e59a0e715ebae34
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-10-05 14:19:56 -0300

    exe19 refactored

--- 3.EstruturaDeRepeticao/19-maior-e-menor-v2.py
+++ 3.EstruturaDeRepeticao/19-maior-e-menor-v2.py
@@ -2,19 +2,21 @@
 
 # Altere o programa anterior para que ele aceite apenas números entre 0 e 1000.
 
-list1 = []
-len_list1 = int(input('Tamanho da lista: '))
+lista = []
+qty_num = int(input('Quantidade de números: '))
 
-for i in range(len_list1):
+for i in range(qty_num):
     nums = int(input(f'Digite o {i+1}º número: '))
 
     while nums < 0 or nums > 1000:
         nums = int(input(f'ERRO!! Digite o {i+1}º número: '))
     else:
-        list1.append(nums)
+         lista.append(nums)
 
 print(f'''
-       O maior número é {max(list1)}
-       O menor número é {min(list1)}
-       E sua soma total é {sum(list1)}
-       ''')
+
+O maior número é {max(lista)}
+O menor número é {min(lista)}
+E sua soma total é {sum(lista)}
+
+''')
