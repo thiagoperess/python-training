0da31345bd2142102cf2de4186b16715ccd14d22
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-10-05 14:20:21 -0300

    exe22 refactored

--- 3.EstruturaDeRepeticao/22-numeros-primos-v2.py
+++ 3.EstruturaDeRepeticao/22-numeros-primos-v2.py
@@ -4,19 +4,17 @@
 # caso o número não seja primo, por quais número ele é divisível.
 
 count = 0
+divide = []
 num = int(input('Digite um número: '))
 
 for i in range(1, num + 1):
-    if num % i == 0:
-        print('\033[32m', end='')
-        count += 1
-    else:
-        print('\033[33m', end='')
-    print(f'{i}', end=' ')
-    
-print(f'\n\033[mO número {num} foi disível {count} vezes.')
 
-if count == 2:
-    print('Portanto, este é um número primo.')
-else:
-    print('Logo, este não é um número primo.')
+    if num % i == 0:
+        divide.append(i)
+
+    else:
+        count += 1
+        
+print(f'Divisores de {num}: {divide}')
+    
+print(f'{num} é primo.' if count == 2 else f'{num} não é primo.')
