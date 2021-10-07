a4b37dcf79243f307132b696595ad1b7cf7d3911
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-10-07 10:05:35 -0300

    exe23 refactored

--- 3.EstruturaDeRepeticao/23-numeros-primos-v3.py
+++ 3.EstruturaDeRepeticao/23-numeros-primos-v3.py
@@ -1,23 +1,24 @@
 # Exercício 23
 
-# Faça um programa que mostre todos os primos entre 1 e N sendo N um número inteiro fornecido pelo usuário. 
-# O programa deverá mostrar também o número de divisões que ele executou para encontrar os números primos. 
+# Faça um programa que mostre todos os números entre 1 e N 
+# sendo N um número inteiro fornecido pelo usuário. Destaque os números 
+# divisíveis com caracteres ANSI, e responda se ele é primo ou não. 
+
 # Serão avaliados o funcionamento, o estilo e o número de testes (divisões) executados.
 
 count = 0
+divide = []
 num = int(input('Digite um número: '))
 
 for i in range(1, num + 1):
     if num % i == 0:
         print('\033[32m', end='')
         count += 1
+        divide.append(i)
+        
     else:
         print('\033[33m', end='')
     print(f'{i}', end=' ')
     
-print(f'\n\033[mO número {num} foi disível {count} vezes.')
-
-if count == 2:
-    print('Portanto, este é um número primo.')
-else:
-    print('Logo, este não é um número primo.')
+print(f'\n\033[mO número {num} foi disível {count} vezes.')    
+print(f'{num} é primo.' if len(divide) == 2 else f'{num} não é primo.')
