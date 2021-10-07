47732dbc6b06cbc1eb8264bccf3be0e1435ef806
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-10-07 10:05:18 -0300

    exe22 refactored

--- 3.EstruturaDeRepeticao/22-numeros-primos-v2.py
+++ 3.EstruturaDeRepeticao/22-numeros-primos-v2.py
@@ -17,4 +17,4 @@ for i in range(1, num + 1):
         
 print(f'Divisores de {num}: {divide}')
     
-print(f'{num} é primo.' if count == 2 else f'{num} não é primo.')
+print(f'{num} é primo.' if len(divide) == 2 else f'{num} não é primo.')
