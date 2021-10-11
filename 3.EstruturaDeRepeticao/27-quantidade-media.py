0538a7aab9aed2d2fc83684d704b01378f0d5ba4
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-10-11 00:21:25 -0300

    exe27 refactored

--- 3.EstruturaDeRepeticao/27-quantidade-media.py
+++ 3.EstruturaDeRepeticao/27-quantidade-media.py
@@ -1,23 +1,21 @@
 # Exercício 27
 
-# Faça um programa que calcule o número médio de alunos por turma. 
-# Para isto, peça a quantidade de turmas e a quantidade de alunos para cada turma. 
-#  As turmas não podem ter mais de 40 alunos.
+# Faça um programa que calcule o número médio de estudantes por turma. 
+# Para isto, peça a quantidade de turmas e a quantidade de estudantes para cada turma. 
+#  As turmas não podem ter mais de 40 estudantes.
 
 import numpy as np
 
 classes = int(input('Quantidade de turmas: '))
 
-total_student = []
+total = []
 for i in range(classes):
-    alunos = int(input(f'Alunos da {i+1}ª turma: '))
+    students = int(input(f'Estudantes da {i+1}ª turma: '))
     
-    while alunos > 40:
-        alunos = int(input(f'Só 40 na {i+1}ª turma: '))    
+    while students > 40:
+        students = int(input(f'Só 40 por turma: '))    
     
-    total_student.append(alunos)
+    total.append(students)
 
-print(total_student)
-
-mean = np.mean(total_student)
-print(f'{mean:.2f}')
+media = np.mean(total)
+print(f'Média de estudantes: {media:.0f}')
