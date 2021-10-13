4653890b6285ef36cabd6b6b033c81686f1d404c
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-10-13 20:23:43 -0300

    exe47

---
+++ 3.EstruturaDeRepeticao/47-analise-atleta-v2.py
@@ -0,0 +1,46 @@
+# Exercício 47
+
+# Em uma competição de ginástica, cada atleta recebe votos de sete jurados. 
+# A melhor e a pior nota são eliminadas. A sua nota fica sendo a média dos votos restantes. 
+# Você deve fazer um programa que receba o nome do ginasta e as notas dos sete jurados 
+# alcançadas pelo atleta em sua apresentação e depois informe a sua média, conforme a descrição 
+# acima informada (retirar o melhor e o pior salto e depois calcular a média com as notas restantes). 
+# As notas não são informados ordenadas. Um exemplo de saída do programa deve ser conforme o exemplo abaixo:
+
+# Atleta: Aparecido Parente
+# Nota: 9.9
+# Nota: 7.5
+# Nota: 9.5
+# Nota: 8.5
+# Nota: 9.0
+# Nota: 8.5
+# Nota: 9.7
+
+# Resultado final:
+# Atleta: Aparecido Parente
+# Melhor nota: 9.9
+# Pior nota: 7.5
+# Média: 9,04
+
+nome = str(input('Nome: '))
+
+notas = []
+for i in range(7):
+    nota = float(input(f'{i+1}ª nota: '))
+    notas.append(nota)
+
+print(
+      f'\nResultado Final:'
+      f'\nAtleta: {nome}'
+      f'\nMelhor nota: {max(notas)}'
+      f'\nPior nota: {min(notas)}'
+      )
+
+notas.remove(min(notas))
+notas.remove(max(notas))
+
+media = sum(notas) / len(notas)
+
+print(
+      f'Média Geral: {media}'
+      )
